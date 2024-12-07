7c5abe0779a65d5aac5cc7dc0f49ed73e16c9c5b
Aditya Bapat <adityahbapat@gmail.com>
2024-12-07 15:12:56 +0530

    fix: changes to sol

--- backend/contracts/ToolRegistry.sol
+++ backend/contracts/ToolRegistry.sol
@@ -2,16 +2,16 @@
 pragma solidity ^0.8.0;
 
 contract ToolRegistry {
-  struct Social {
-    string socialType;
-    string url;
-    Project[] projects;
-  }
-
   struct Project {
     address creatorId;
     string creatorGithubProfile;
     string repoUrl;
+  }
+
+  struct Social {
+    string socialType;
+    string url;
+    Project[] projects;
   }
 
   struct Tool {
@@ -26,25 +26,77 @@ contract ToolRegistry {
   uint256 public toolCount;
   mapping(uint256 => Tool) public tools;
 
-  function addTool(
-    string memory _image,
-    string[] memory _keywords,
-    string memory _repoLink,
-    string memory _docsLink,
-    Social[] memory _socials
-  ) public {
-    toolCount++;
-    tools[toolCount] = Tool({
-      id: toolCount,
-      image: _image,
-      keywords: _keywords,
-      repoLink: _repoLink,
-      docsLink: _docsLink,
-      socials: _socials
-    });
+  struct ToolInput {
+    string image;
+    string[] keywords;
+    string repoLink;
+    string docsLink;
+    Social[] socials;
+    Project[][] projectsPerSocial;
   }
 
-  function getTool(uint256 toolId) public view returns (Tool memory) {
+  function addTool(ToolInput calldata toolInput) external {
+    require(toolInput.keywords.length > 0, 'Keywords cannot be empty');
+    require(
+      toolInput.socials.length == toolInput.projectsPerSocial.length,
+      'Mismatched socials and projects'
+    );
+
+    toolCount++;
+
+        // Initialize the new Tool in storage
+    Tool storage newTool = tools[toolCount];
+    newTool.id = toolCount;
+
+    // Store basic tool information
+    _storeBasicToolInfo(
+      newTool,
+      toolInput.image,
+      toolInput.repoLink,
+      toolInput.docsLink,
+      toolInput.keywords
+    );
+
+    // Store socials and their projects
+    _storeSocials(newTool, toolInput.socials, toolInput.projectsPerSocial);
+  }
+
+  function _storeBasicToolInfo(
+    Tool storage tool,
+    string calldata _image,
+    string calldata _repoLink,
+    string calldata _docsLink,
+    string[] calldata _keywords
+  ) internal {
+    tool.image = _image;
+    tool.repoLink = _repoLink;
+    tool.docsLink = _docsLink;
+
+    for (uint256 i = 0; i < _keywords.length; i++) {
+      tool.keywords.push(_keywords[i]);
+    }
+  }
+
+  function _storeSocials(
+    Tool storage tool,
+    Social[] calldata _socials,
+    Project[][] calldata _projectsPerSocial
+  ) internal {
+    for (uint256 i = 0; i < _socials.length; i++) {
+      tool.socials.push();
+      Social storage storedSocial = tool.socials[tool.socials.length - 1];
+      storedSocial.socialType = _socials[i].socialType;
+      storedSocial.url = _socials[i].url;
+
+      for (uint256 j = 0; j < _projectsPerSocial[i].length; j++) {
+        storedSocial.projects.push(_projectsPerSocial[i][j]);
+      }
+    }
+  }
+
+  // Retrieve a tool by its ID
+  function getTool(uint256 toolId) external view returns (Tool memory) {
+    require(toolId > 0 && toolId <= toolCount, 'Tool ID out of range');
     return tools[toolId];
   }
 }
